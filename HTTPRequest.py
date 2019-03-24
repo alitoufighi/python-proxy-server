d4c15150386c871edbf474bc528f26d52536125d
Mohammad Ali Toufighi <alitoumob@gmail.com>
2019-03-24 17:19:05 +0430

    refactor

--- HTTPRequest.py
+++ HTTPRequest.py
@@ -4,10 +4,10 @@ import socket
 class BadRequest(Exception):
     def __init__(self, msg):
         Exception.__init__(self)
-        print('bad http request: {0}'.format(msg))
+        print(f'bad http request: {msg}')
 
 
-class IncomingHTTPRequest:
+class HTTPRequest:
 
     DEFAULT_ENCODING = "iso-8859-1"
 
@@ -15,38 +15,25 @@ class IncomingHTTPRequest:
         self.socket = sock
         self.fp = sock.makefile('rb', 0)
 
-        self.method, self.route, self.version = self.read_status()
-        self.headers = self.read_headers()
-        self.body = self.read_body()
+        self.method, self.route, self.version = self._read_status()
+        self.headers = self._read_headers()
+        self.body = self._read_body()
 
         self.modify_values_for_proxy()
 
-        # print('------------------BEG OF REQUEST----------------')
-        # print(self.method, self.route, self.version)
-        # print(self.headers)
-        # print(self.body)
-        # print('------------------END OF REQUEST----------------')
-
-    def remove_accept_gzip_encoding(self):
-        if not ('accept-encoding' in self.headers):
-            return
-        accept_encodings = self.headers['accept-encoding']
-        accept_encodings = accept_encodings.replace('gzip', '')
-        self.headers['accept-encoding'] = accept_encodings.strip(', ')
-
-    def read_status(self):
-        line = self.fp.readline().decode(IncomingHTTPRequest.DEFAULT_ENCODING)
+    def _read_status(self):
+        line = self.fp.readline().decode(HTTPRequest.DEFAULT_ENCODING)
         if not line:
             raise BadRequest('not a valid line')
         method, route, version = line.split(None, 2)
         version = version.strip()
         return method, route, version
 
-    def read_headers(self):
+    def _read_headers(self):
         headers = {}
         while True:
-            line = self.fp.readline().decode(IncomingHTTPRequest.DEFAULT_ENCODING)
-            if self.is_last_header(line):
+            line = self.fp.readline().decode(HTTPRequest.DEFAULT_ENCODING)
+            if self._is_last_header(line):
                 break
             try:
                 key, value = self.extract_header(line)
@@ -55,13 +42,31 @@ class IncomingHTTPRequest:
             headers[key] = value
         return headers
 
-    def extract_header(self, line):
-        i = line.find(':')
-        if i < 0:
-            raise BadRequest("not a valid header")
-        key = line[:i].lower()
-        val = line[i+1:].strip()
-        return key, val
+    def _is_last_header(self, line):
+        return True if line in ['\r\n', '\n'] else False
+
+    def _read_body(self):
+        data = ''
+        if self.length > 0:
+            try:
+                data = self.fp.read().decode(HTTPRequest.DEFAULT_ENCODING)
+            except socket.error:
+                raise BadRequest('socket error in read_body')
+        return data
+
+    def remove_accept_gzip_encoding(self):
+        if not ('accept-encoding' in self.headers):
+            return
+        accept_encodings = self.headers['accept-encoding']
+        accept_encodings = [item.strip() for item in accept_encodings.split(',')]
+        try:
+            accept_encodings.remove('gzip')
+        except ValueError:
+            pass
+        self.headers['accept-encoding'] = accept_encodings
+
+    def set_header(self, key, val):
+        self.headers[key.lower()] = val.strip()
 
     def get_header(self, key):
         try:
@@ -69,17 +74,13 @@ class IncomingHTTPRequest:
         except KeyError:
             return None
 
-    def is_last_header(self, line):
-        return True if line in ['\r\n', '\n'] else False
-
-    def read_body(self):
-        data = ''
-        if self.length > 0:
-            try:
-                data = self.fp.read().decode(IncomingHTTPRequest.DEFAULT_ENCODING)
-            except socket.error:
-                raise BadRequest('socket error in read_body')
-        return data
+    def extract_header(self, line):
+        i = line.find(':')
+        if i < 0:
+            raise BadRequest("not a valid header")
+        key = line[:i].lower()
+        val = line[i+1:].strip()
+        return key, val
 
     def modify_values_for_proxy(self):
         self.version = 'HTTP/1.0'
@@ -91,15 +92,12 @@ class IncomingHTTPRequest:
         self.remove_accept_gzip_encoding()
 
     def read(self):
-        result = '{0} {1} {2}\r\n'.format(self.method, self.route, self.version)
+        result = f'{self.method} {self.route} {self.version}\r\n'
         for key, value in self.headers.items():
-            result += '{0}: {1}\r\n'.format(key, value)
-        result += '\r\n{0}\r\n'.format(self.body)
-        return result.encode(IncomingHTTPRequest.DEFAULT_ENCODING)
+            result += f'{key}: {value}\r\n'
+        result += f'\r\n{self.body}\r\n'
+        return result.encode(HTTPRequest.DEFAULT_ENCODING)
 
     @property
     def length(self):
         return int(self.headers['content-length']) if 'content-length' in self.headers else 0
-
-    def set_header(self, key, val):
-        self.headers[key.lower()] = val.strip()
--- HTTPRequestHandler.py
+++ HTTPRequestHandler.py
@@ -1,6 +1,6 @@
 from HTTPResponse import *
 from HTTPRequest import *
-# from email import send_mail
+# from mail import send_mail
 
 
 class HTTPRequestHandler(object):
@@ -8,37 +8,28 @@ class HTTPRequestHandler(object):
 
     @staticmethod
     def run(proxy_server, sock, addr):
-        try:
-            req = IncomingHTTPRequest(sock)
-        except BadRequest:
-            print("That was a bad request!")
-            sock.close()
-            return
+        request = HTTPRequest(sock)
 
-        host = req.get_header('Host')
+        host = request.get_header('Host')
 
         if proxy_server.is_restriction_enabled() and proxy_server.is_in_disallowed_hosts(host):
-            # Send Email to Administrator
             print("-Sending Email to Admin!")
-
-            # Uncomment lines below to make it functional
-            # sock.close()
+            sock.sendall(proxy_server.bad_response('Visiting this site is forbidden!').read())
+            sock.close()
             # send_mail().snd_email()
-            # return
+            return
 
         if proxy_server.is_privacy_enabled():
-            # Privacy Mechanism
-            req.set_header('user-agent', proxy_server.privacy_user_agent)
+            request.set_header('user-agent', proxy_server.privacy_user_agent)
 
         server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server_socket.connect((host, HTTPRequestHandler.HTTP_SERVER_LISTENING_PORT))
-        server_socket.send(req.read())
+        server_socket.send(request.read())
 
         response = HTTPResponse(server_socket)
         server_socket.close()
 
         if response.is_html() and proxy_server.is_http_injection_enabled():
-            # HTTP-Injection
             response.body = proxy_server.body_inject(response.body)
 
         proxy_server.discharge_user(addr, response.length)
